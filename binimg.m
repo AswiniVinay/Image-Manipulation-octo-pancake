8ec9f387c13a1118bc627ef509c43cc55afb2a7c
Aswini Vinay Soreng <aswin.soreng@gmail.com>
2021-05-22 14:08:03 +0530

    Add files via upload
    Converting a grayscale image to binary

---
+++ binimg.m
@@ -0,0 +1,20 @@
+I=imread("Baboon.png");
+
+[h w] = size(I);
+
+avg =floor(mean2(I))
+
+for i=1:h
+   for j=1:w
+     
+     if (I(i,j)<avg)
+       A(i,j) = uint8(0);
+     else
+       A(i,j) = uint8(255);
+     endif
+     
+   endfor
+   
+endfor
+
+imshow(A);
